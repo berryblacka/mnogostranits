9e87bdd2a8cbbdafd65aaaf336ca8f77d30f400f
yar1kukharenko <116860258+berryblacka@users.noreply.github.com>
2023-06-23 12:44:04 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,68 +1,7 @@
+/* eslint-disable no-new */
 /* eslint-disable no-undef */
 // /* eslint-disable no-undef */
 // /* eslint-disable no-new */
-// $(document).ready(() => {
-//   $('.house-img').on('mouseover', () => {
-//     $('#text1').animate(
-//       {
-//         translateX: ['-10px', '10px', '-5px', '5px', '0'],
-//       },
-//       1000,
-//       'easeInOutQuad',
-//     );
-//   });
-
-//   $('.tree-img').on('mouseover', () => {
-//     $('#text2').animate(
-//       {
-//         translateX: ['-10px', '10px', '-5px', '5px', '0'],
-//       },
-//       1000,
-//       'easeInOutQuad',
-//     );
-//   });
-
-//   $('.vazy-img').on('mouseover', () => {
-//     $('#text3').animate(
-//       {
-//         translateX: ['-10px', '10px', '-5px', '5px', '0'],
-//       },
-//       1000,
-//       'easeInOutQuad',
-//     );
-//   });
-
-//   $('.flowers-img').on('mouseover', () => {
-//     $('#text4').animate(
-//       {
-//         translateX: ['-10px', '10px', '-5px', '5px', '0'],
-//       },
-//       1000,
-//       'easeInOutQuad',
-//     );
-//   });
-
-//   if (window.Parallax) {
-//     new Parallax($('#scene_house_sq1')[0], { invertX: false, invertY: false });
-//     new Parallax($('#scene_house_sq2')[0]);
-//     new Parallax($('#scene_vazy_sq1')[0], { invertX: true, invertY: true });
-//     new Parallax($('#scene_vazy_sq2')[0], { invertX: false, invertY: true });
-//     new Parallax($('#scene_tree_sq1')[0], { invertX: false, invertY: true });
-//     new Parallax($('#scene_tree_sq2')[0]);
-//     new Parallax($('#scene_flowers_sq1')[0], { invertX: false, invertY: false });
-//   }
-// });
-
-/* eslint-disable no-new */
-// const house = document.querySelector('.house-img');
-// const tree = document.querySelector('.tree-img');
-// const vazy = document.querySelector('.vazy-img');
-// const flowers = document.querySelector('.flowers-img');
-
-// const text1 = document.getElementById('text1');
-// const text2 = document.getElementById('text2');
-// const text3 = document.getElementById('text3');
-// const text4 = document.getElementById('text4');
 
 $(document).ready(() => {
   $('.house-img').on('mouseover', () => {
@@ -93,7 +32,7 @@ $(document).ready(() => {
       duration: 1000,
     });
   });
-  $('.flowers-img').on('mouseover', () => {
+  $('.vazy-img').on('mouseover', () => {
     anime({
       targets: '#text3',
       translateX: [
